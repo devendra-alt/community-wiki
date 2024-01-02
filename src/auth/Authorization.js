5b32e45371cf8aca1236886f4b3bbd0b157e1c70
devendra-alt <devendramulewa171@gmail.com>
2024-01-02 13:40:18 +0530

    Add auth and gql setup

---
+++ src/auth/Authorization.js
@@ -0,0 +1,16 @@
+import { ApolloProvider } from '@apollo/client';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { client } from './client';
+import Landing from '../pages/landing';
+import App from '../App';
+
+export default function Authorization() {
+  return (
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        {localStorage.getItem('token') ? <Landing /> : <App />}
+      </ApolloProvider>
+    </BrowserRouter>
+  );
+}
