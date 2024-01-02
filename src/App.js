a48e7f316ff9bca6ebc2198684aa971d34143f31
devendra-alt <devendramulewa171@gmail.com>
2024-01-02 13:39:35 +0530

    Add App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ResponsiveAppBar from './source/home/header';
+import Content from './layout/content';
+
+export default function App() {
+  return (
+    <>
+      <ResponsiveAppBar />
+      <Content />
+    </>
+  );
+}
