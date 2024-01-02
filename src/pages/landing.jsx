faa0c041e85c64c7fa1ca0e3dde32e9d8bdefa91
devendra-alt <devendramulewa171@gmail.com>
2024-01-02 13:31:03 +0530

    Add  landing page

---
+++ src/pages/landing.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ResponsiveAppBar from '../source/landing/header';
+
+export default function Landing() {
+  return (
+    <h1>
+      <ResponsiveAppBar />
+    </h1>
+  );
+}
