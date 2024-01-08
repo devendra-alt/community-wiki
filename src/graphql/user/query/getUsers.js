20653813f6f054e5bff3229ed0432a818a778bf1
devendra-alt <devendramulewa171@gmail.com>
2024-01-08 18:56:28 +0530

    Add required fields for getUser

--- src/graphql/user/query/getUsers.js
+++ src/graphql/user/query/getUsers.js
@@ -6,14 +6,58 @@ const GET_USERS = gql`
       data {
         id
         attributes {
-          username
+          firstname
+          lastname
+          father
+          marital
+          sex
+          gotra
           email
+          username
+          mobile
+          dob
+          education_level
           photo {
             data {
               id
               attributes {
                 name
                 formats
+              }
+            }
+          }
+          business_profiles {
+            data {
+              id
+              attributes {
+                name
+                type
+                subtype
+                name
+                startdate
+                establised_date
+                size
+                logo {
+                  data {
+                    id
+                    attributes {
+                      formats
+                    }
+                  }
+                }
+                addresses {
+                  data {
+                    id
+                    attributes {
+                      addresstype
+                      address_raw
+                      latitude
+                      longitude
+                      pincode
+                      district
+                    }
+                  }
+                }
               }
             }
           }
