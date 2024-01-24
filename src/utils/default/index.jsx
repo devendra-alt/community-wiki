dfa2340c6e83f2db31f5cf51ec81699247523e63
devendra-alt <devendramulewa171@gmail.com>
2024-01-24 14:18:27 +0530

    Added defaultRender component

---
+++ src/utils/default/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function DefaultRender() {
+  return (
+    <div
+      style={{
+        textAlign: 'center',
+        marginTop: '100px',
+        fontSize: '28px',
+        fontWeight: 'bold',
+      }}
+    >
+      You are not authorized! Please go back
+    </div>
+  );
+}
