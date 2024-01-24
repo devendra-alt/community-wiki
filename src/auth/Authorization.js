fa5e49203e7de2d3238fa12c68a329bef9f25ddd
devendra-alt <devendramulewa171@gmail.com>
2024-01-24 14:22:50 +0530

    Add isLoggedIn flag for render home

--- src/auth/Authorization.js
+++ src/auth/Authorization.js
@@ -4,12 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { client } from './client';
 import Landing from '../pages/landing';
 import App from '../App';
+import { useSelector } from 'react-redux';
 
 export default function Authorization() {
+  const { isLoggedIn } = useSelector((state) => state.auth);
   return (
     <BrowserRouter>
       <ApolloProvider client={client}>
-        {localStorage.getItem('token') ? <App /> : <Landing />}
+        {localStorage.getItem('token') || isLoggedIn ? <App /> : <Landing />}
       </ApolloProvider>
     </BrowserRouter>
   );
